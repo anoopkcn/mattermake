d5992dbab9fbbfdf8754bf549a1a8ea844e0eeaf
Anoop K. Chandran <anoopkcn@gmail.com>
2025-04-12 09:05:36 +0200

    save load tokenizer_config alsong with model instead of saving it seperately

--- mattermake/models/hierarchical_crystal_transformer_module.py
+++ mattermake/models/hierarchical_crystal_transformer_module.py
@@ -78,6 +78,16 @@ class HierarchicalCrystalTransformerModule(LightningModule):
 
         # Track current epoch for curriculum learning
         self.current_curriculum_epoch = 0
+        
+    def on_save_checkpoint(self, checkpoint):
+        """Save tokenizer config with checkpoint"""
+        if self.tokenizer_config:
+            checkpoint['tokenizer_config'] = self.tokenizer_config
+            
+    def on_load_checkpoint(self, checkpoint):
+        """Load tokenizer config from checkpoint"""
+        if 'tokenizer_config' in checkpoint:
+            self.tokenizer_config = checkpoint['tokenizer_config']
 
     def forward(self, batch):
         """Forward pass"""
